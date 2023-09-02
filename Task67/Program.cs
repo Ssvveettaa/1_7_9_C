e151916919d4081dee9e6f2f47d39313f4b6fefb
Svetlana Panina <paninasi@inbox.ru>
2023-09-03 00:40:56 +0300

    Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67:
+// Напишите программу, которая
+// будет принимать на вход число
+// и возвращать сумму его цифр.
+// 453 –> 12
+//  45 –> 9
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumOfDigits(number));
+
+int SumOfDigits(int num) // Параметр: 453, 45, 4, 0
+{
+    if (num == 0) return 0; // Условие окончания рекурсии: все разряды перебрали.
+    return num % 10 + SumOfDigits(num / 10); // Стек: 453, 45, 4
+} // return: 0 + 4 % 10 + 45 % 10 + 453 % 10

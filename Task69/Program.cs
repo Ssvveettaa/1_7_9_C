8b673a47342d452a68fb85b02ecd7632a267e3a1
Svetlana Panina <paninasi@inbox.ru>
2023-09-03 00:41:44 +0300

    Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 69:
+// Напишите программу, которая
+// на вход принимает два числа A и B,
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 –> 243 (3^5)
+// A = 2; B = 3 –> 8
+
+Console.Write("Введите целое число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB < 1)
+{
+    Console.WriteLine("Число B не натуральное.");
+    return;
+}
+
+Console.WriteLine(PowNum(numberA, numberB));
+
+int PowNum(int numA, int numB)
+{
+    if (numB == 0) return 1;
+    return numA * PowNum(numA, numB - 1);
+}
